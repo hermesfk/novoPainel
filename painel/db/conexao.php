d76fb597766f2cfa2a474b418e3e46a2c617eaa1
PROG WEB - MANHA <PROG WEB - MANHA@MININT-GITGFLV>
2021-01-27 11:50:17 -0300

    PainelAdmin ATUALIZAÇÃO 25/01

---
+++ painel/db/conexao.php
@@ -0,0 +1,21 @@
+<?php
+
+class conexao {
+
+    private $db_server = 'localhost';
+    private $db_name = 'paineladm';
+    private $db_charset = 'utf8';
+    private $db_username = 'root';
+    private $db_password = '';
+
+    public function selecionardados($consulta, $parametros = null, $debug = true, $closse_connection = true) {
+        
+        $result = NULL;
+        $connection = new PDO(
+                
+                
+                );
+        
+    }
+
+}
