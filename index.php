cae561863dcd4ac8b0361ff19ed2f3dc139e0dd6
PROG WEB - MANHA <PROG WEB - MANHA@MININT-GITGFLV>
2021-01-29 11:57:01 -0300

    PainelAdmin ATUALIZAÇÃO 29/01 ultima att !

--- index.php
+++ index.php
@@ -41,28 +41,31 @@ if ($pg) {
             include_once 'painel/paginas/includes/footer.php';
             break;
 
-        
+
         case 'produtos-editar':
-            
+
             include_once 'painel/paginas/includes/header.php';
             include_once 'painel/paginas/includes/menus.php';
-            
-            if ($_SERVER['REQUEST_METHOD']=='POST') {
-            //FUNÇÃO PARA ATUALIZAÇÃO DO USUÁRIO
-                
-                
+
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //FUNÇÃO PARA ATUALIZAÇÃO DO USUÁRIO
             } else {
                 //MOSTRAR OS DADOS DO PRODUTO
+                $idProdutoEditar = isset($_GET['id']);
+                if ($idProdutoEditar) {
+                    $resultDados = new conexao();
+                    $dados = $resultDados->selecionaDados('SELECT * FROM produtos WHERE  id = ' . $idProdutoEditar);
+                    include_once 'painel/paginas/produtos-editar.php';
+                } 
             }
-            
-            include_once 'painel/paginas/produtos-editar.php';
+
             include_once 'painel/paginas/includes/footer.php';
-           
+
             break;
-        
-               
-            
-            
+
+
+
+
         case 'servicos':
             $resultDados = new conexao();
             $dados = $resultDados->selecionaDados('SELECT * FROM servicos');
@@ -74,8 +77,8 @@ if ($pg) {
             break;
 
         case 'servicos-item':
-             $id = $_GET ['id'];
-            
+            $id = $_GET ['id'];
+
             $resultDados = new conexao();
             $dados = $resultDados->selecionaDados('SELECT * FROM servicos WHERE  id = ' . $id);
 
--- painel/paginas/produtos-editar.php
+++ painel/paginas/produtos-editar.php
@@ -29,7 +29,7 @@
             <!-- jquery validation -->
             <div class="card card-success">
               <div class="card-header">
-                <h3 class="card-title">Quick Example <small>jQuery Validation</small></h3>
+                <h3 class="card-title">Nome Do Produto</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
